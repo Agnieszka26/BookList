4c44c40b8b99ce927a315d1c3cd40d5bfd3e5a8f
Agnieszka26 <76781871+Agnieszka26@users.noreply.github.com>
2022-01-24 14:59:27 +0100

    first commit

---
+++ src/Component/BookDetail.js
@@ -0,0 +1,14 @@
+import react from "react";
+import {useContext} from "react/cjs/react.development";
+import {BookContext} from "../context/BookContext";
+
+const BookDetails = ({book}) => {
+  const {removeBook} = useContext(BookContext);
+  return (
+    <li>
+      <div> {book.title} </div> <div> {book.author}</div>{" "}
+    </li>
+  );
+};
+
+export default BookDetails;
